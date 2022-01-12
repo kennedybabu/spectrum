f75a04b7a00fa4bb8598b571c792f37874400714
kennedybabu <kennedynjogu8@gmail.com>
2022-01-12 05:23:52 +0300

    feature: create the login page

--- spectrum/urls.py
+++ spectrum/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('login/', views.loginPage, name='login'),
+    path('', views.home, name='home'),
+    path('interest/<int:pk>/', views.interest, name='interest' )
 ]
