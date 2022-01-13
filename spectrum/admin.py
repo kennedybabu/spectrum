0ac2475b5137b1d94395895c187f5e0241effa18
kennedybabu <kennedynjogu8@gmail.com>
2022-01-13 04:49:39 +0300

    register the models in the admin panel

--- spectrum/admin.py
+++ spectrum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Interest, Post, Comment
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Interest)
+admin.site.register(Comment)
+admin.site.register(Post)
