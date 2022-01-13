97d01cdc4c9d8fbccc0b41a7bb5f88c082b93c51
kennedybabu <kennedynjogu8@gmail.com>
2022-01-13 12:06:04 +0300

    feature: register a user into the application

--- spectrum/urls.py
+++ spectrum/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerUser, name='register'),
     path('', views.home, name='home'),
     path('interest/<int:pk>/', views.interest, name='interest' ),
     path('create_interest/', views.createInterest, name='create_interest'),
